4fdbf391efb77c1cba2bdeb2c7c2802824c136a7
danylo.volokh <v.danylo@gmail.com>
2015-12-11 02:58:28 +0200

    disabled logs

--- app/src/main/java/com/volokh/danylo/utils/Config.java
+++ app/src/main/java/com/volokh/danylo/utils/Config.java
@@ -4,5 +4,5 @@ package com.volokh.danylo.utils;
  * Created by danylo.volokh on 10/17/2015.
  */
 public class Config {
-    public static final boolean SHOW_LOGS = true;
+    public static final boolean SHOW_LOGS = false;
 }
